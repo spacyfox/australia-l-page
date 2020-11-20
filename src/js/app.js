37a5949136f1014586ca96ccd2be51e612a73761
spacyfox <spacyfox@gmail.com>
2020-11-20 11:35:47 +0300

    './modules/sliders' error

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,19 @@
-import './modules/sitePreloader';
+
 import documentReady from './utils/documentReady';
 import documentLoaded from './utils/documentLoaded';
 
+import sitePreloader from './modules/sitePreloader';
 import cssVars from './modules/cssVars';
 import resize from './modules/resize';
 import lazyload from './modules/lazyload';
+import sliders from './modules/sliders';
 
 documentReady(() => {
+  sitePreloader.init();
   cssVars.init();
   resize.init();
   lazyload.init();
+  sliders.init();
 });
 
 documentLoaded(() => {
