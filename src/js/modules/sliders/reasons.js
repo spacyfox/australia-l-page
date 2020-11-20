f92dc63c117b7160d0fd8dc577493c15771fced0
spacyfox <spacyfox@gmail.com>
2020-11-20 14:01:53 +0300

    './modules/sliders' error.

---
+++ src/js/modules/sliders/reasons.js
@@ -0,0 +1,32 @@
+import swiper from 'swiper/bundle';
+
+function initReasonsSlider() {
+  const reasonsSlider = document.querySelectorAll('.s-reasons__slider');
+
+  function init(el) {
+    return new swiper('.s-reasons__slider');
+  }
+
+  Array.prototype.forEach.call(reasonsSlider, (el, i)=> {
+    let sliderInstance = null;
+    const mql = window.matchMedia('(max-width: 767px)');
+
+    if (mql.matches) {
+      sliderInstance = init(el);
+    }
+
+    mql.addEventListener('change', (e) => {
+      if (e.matches) {
+        sliderInstance = init(el);
+      } else if (sliderInstance) {
+        sliderInstance.destroy();
+      }
+    });
+  });
+}
+
+function init() {
+  initReasonsSlider();
+}
+
+document.addEventListener('DOMContentLoaded', init);
